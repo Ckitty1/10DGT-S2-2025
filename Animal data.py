2df9983380730f85e8adc9f4b42bbc29f53132a3
Ckitty1 <luke.low@kingsway.school.nz>
2025-10-09 09:36:50 +1300

    Animal data task (chicken)

---
+++ Animal data.py
@@ -0,0 +1,11 @@
+# Storing the data of an animal
+animal_name = "chicken"
+leg_amt = 2
+animal_height = 0.5
+has_tail = True
+
+# displaying the data
+print(f"{animal_name}:")
+print(f"The {animal_name} has {leg_amt} legs.")
+print(f"The {animal_name} is {animal_height}m tall.")
+print(f"The {animal_name} has a tail: {has_tail}.")
