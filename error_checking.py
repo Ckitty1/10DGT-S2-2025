0a9ceae25a20373858f65edee03da0bbd793eaf6
Ckitty1 <luke.low@kingsway.school.nz>
2025-10-24 09:58:09 +1300

    Version 1.4 - Error checking complete
    Make error checking function work properly. Used parameters to make the program more succinct. Plus some additional operations to test

--- error_checking.py
+++ error_checking.py
@@ -1,8 +1,8 @@
 '''
 Author: Luke Low
-Date: 22/10/25
+Date: 24/10/25
 Title: Error checking
-Version 11.0
+Version 1.4
 
 Code tht tests whether a valid inout is given
 '''
@@ -56,7 +56,7 @@ test_int()'''
 # Code that tests whether a valid input is given (v1.3)
 # : using the function parameters to make my code more pythonic.
 
-def test_int(question):
+'''def test_int(question):
     done = False
     error = "That is not a valid integer."
 
@@ -80,4 +80,40 @@ num2 = test_int("Please enter your second number: ")
 print(f"Your second number you entered is {num2}.\n")
 
 sum = num1 + num2
-print(f"Your two numbers added is {sum}.\n")
+print(f"Your two numbers added is {sum}.\n")'''
+
+# Code that tests whether a valid input is given (v1.4)
+# : using the function parameters to make my code more pythonic.
+
+def valid_num(question, low, high):
+    error = f"Whoops, that is not an integer between {low} and {high}.\n"
+
+    while True:
+        try:
+            response = int(input(f"{question}\n"))
+            
+            if low <= response <= high:
+                break # stops the while loop
+            
+            else: 
+                print(error)
+
+        except ValueError:
+            print(error)
+
+    return(response)
+
+num1 = valid_num("Enter a number between 1 and 10: ", 1, 10)
+print(f"You entered {num1}.\n")
+
+num2 = valid_num("Enter a number between 15 and 25: ", 15, 25)
+print(f"You entered {num2}.\n")
+
+num3 = valid_num("Enter a number between 70 and 90", 70, 90)
+print(f"You entered {num3}.\n")
+
+multiply = num1 * num2 * num3
+print(f"Your three nunmbers multiplied together are equal to {multiply}.\n")
+
+sum = num1 + num2 + num3
+print(f"The total of {num1}, {num2} and {num3} is {sum}.\n")
